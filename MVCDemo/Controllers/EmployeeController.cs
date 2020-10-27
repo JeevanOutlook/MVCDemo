143c0ad57cdc2a3468018495fd1d87f2061a5c2c
JC53991 <jeevan.0111@outlook.com>
2020-10-27 15:50:03 +0530

    first commit

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeID = 100,
+                Name = "Jeevan",
+                Gender = "Male",
+                City = "Delhi"
+            };
+
+            return View(employee );
+        }
+    }
+}
